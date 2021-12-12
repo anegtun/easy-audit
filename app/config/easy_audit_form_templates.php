5cab78b102b1dacb9c81ebd90d3ae1002f6e36c1
Angel Negrete <64235746+anegtun@users.noreply.github.com>
2021-12-13 00:28:29 +0100

    CKEditor for rich text (#22)
    Co-authored-by: Angel Negrete <angel.negrete.tunas@gmail.com>

--- app/config/easy_audit_form_templates.php
+++ app/config/easy_audit_form_templates.php
@@ -2,5 +2,6 @@
 return [
     'inputContainer' => '<div class="form-group">{{content}}</div>',
     'input' => '<input type="{{type}}" name="{{name}}" class="form-control {{classes}}"{{attrs}}/>',
-    'select' => '<select name="{{name}}" class="form-control {{classes}}"{{attrs}}>{{content}}</select>'
+    'select' => '<select name="{{name}}" class="form-control {{classes}}"{{attrs}}>{{content}}</select>',
+    'textarea' => '<textarea name="{{name}}" class="form-control {{classes}}"{{attrs}}>{{value}}</textarea>'
 ];
