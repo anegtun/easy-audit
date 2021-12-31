34b6a501efb9869fdb96de5d28768118a7f9237d
Angel Negrete <angel.negrete.tunas@gmail.com>
2021-12-31 15:58:18 +0100

    Improve PDF code

--- app/src/Controller/Component/AuditPdfComponent.php
+++ app/src/Controller/Component/AuditPdfComponent.php
@@ -33,112 +33,84 @@ class AuditPDF extends FPDF {
         }
     }
 
-    function Title($title) {
-        $this->SetFont('Arial', 'B', 20);
-        $this->SetTextColor(29, 113, 184);
-        $this->Cell(0, 20, utf8_decode($title), 0, 0, 'C');
-        $this->SetFont('Arial', '', 12);
-        $this->SetTextColor(0, 0, 0);
-    }
-
     function Cover() {
         // TODO Create image
         $this->Image(WWW_ROOT . DS . 'images' . DS . 'logo' . DS . 'main.png', 50, 50, 100);
         $this->Image(WWW_ROOT . DS . 'images' . DS . 'logo' . DS . 'header.png', 50, 140, 100);
-        $this->SetY(-90);
+        $this->SetY(-95);
         $this->SetFont('Arial', 'B', 25);
         $this->Cell(0, 0, utf8_decode('Informe Auditoría Higiénico-Sanitaria'), 0, 0, 'C');
-        $this->Ln(30);
-        $this->Cell(0, 0, utf8_decode($this->audit->customer->name), 0, 0, 'C');
+        $this->Ln(25);
+        $this->SetFont('Arial', 'B', 20);
+        $this->MultiCell(0, 10, utf8_decode($this->audit->customer->name), 0, 'C');
     }
 
     function MeasureReport($template) {
-        $this->AddPage('P', 'A4', 0);
-        $this->Title('Resultados verificaciones externas');
-        $this->Ln(20);
+        $this->AddPage();
+        $this->Title($template->name);
+        $this->SubTitle('Resultados');
 
-        $this->SetFont('Arial', 'B', 12);
-        $this->SetFillColor(237, 239, 246);
-        $this->SetTextColor(29, 113, 184);
-        $this->Cell(70, 7, utf8_decode('Equipo a verificar'), 1, 0, 'C', true);
-        $this->Cell(15, 7, utf8_decode('Dato'), 1, 0, 'C', true);
-        $this->Cell(25, 7, utf8_decode('M. equipo'), 1, 0, 'C', true);
-        $this->Cell(30, 7, utf8_decode('M. verificada'), 1, 0, 'C', true);
-        $this->Cell(25, 7, utf8_decode('Diferencia'), 1, 0, 'C', true);
-        $this->Cell(25, 7, utf8_decode('Resultado'), 1, 0, 'C', true);
-        $this->Ln(7);
-        $this->SetFillColor(255, 255, 255);
-        $this->SetTextColor(0, 0, 0);
-
-        $this->SetFont('Arial', '', 10);
+        $rows = [
+            [
+                'align' => ['C','C','C','C','C','C'],
+                'bg' => [237,239,246],
+                'color' => [29,113,184],
+                'font' => ['Arial','B',12],
+                'values' => ['Equipo a verificar', 'Dato', 'M. equipo', 'M. verificada', 'Diferencia', 'Resultado']
+            ]
+        ];
         foreach($this->audit->audit_field_measure_values as $m) {
-            $img = $m->isInThreshold() ? 'ok.png' : 'nok.png';
-            $this->Cell(70, 7, utf8_decode($m->item), 1);
-            $this->Cell(15, 7, utf8_decode($m->unit), 1, 0, 'C');
-            $this->Cell(25, 7, $m->expected, 1, 0, 'C');
-            $this->Cell(30, 7, $m->actual, 1, 0, 'C');
-            $this->Cell(25, 7, $m->calculateDifference(), 1, 0, 'C');
-            $x = $this->GetX();
-            $y = $this->GetY();
-            $this->Image(WWW_ROOT . DS . 'images' . DS . 'components' . DS . $img, $x + 10, $y + 1, 5);
-            $this->Cell(25, 7, '', 1, 0, 'C');
-            $this->Ln(7);
-    
+            $img = WWW_ROOT . DS . 'images' . DS . 'components' . DS . ($m->isInThreshold() ? 'ok.png' : 'nok.png');
+            $rows[] = ['values' => [$m->item, $m->unit, $m->expected, $m->actual, $m->calculateDifference(), ['type'=>'img', 'path'=>$img, 'width'=>5]]];
         }
+        $this->Table(
+            $rows,
+            ['align' => ['L','C','C','C','C','C'], 'font' => ['Arial','',10], 'height' => 7, 'width' => [70,15,25,30,25,25]]
+        );
     }
 
     function SelectReport($template) {
         $this->AddPage();
+        $this->Title($template->name);
+        $this->SubTitle('Evaluación de la auditoría');
         $this->SelectReportIntro($template);
         $this->AddPage();
+        $this->SubTitle('Resumen de puntuaciones');
         $this->SelectReportSummary($template);
         $this->AddPage();
+        $this->SubTitle('Detalles de auditoría');
         $this->SelectReportDetail($template);
     }
 
-    function SelectReportIntro($template) {
-        $this->Title('Evaluación de la auditoría');
-        $this->Ln(20);
-        $this->MultiCell(0, 5, utf8_decode('La presente Auditoría Interna Higiénico-Sanitaria se ha evaluado teniendo en cuenta distintos apartados esenciales para llevar a cabo un correcto Sistema de Autocontrol. Este sistema necesario y obligatorio para las empresas alimentarias según el Reglamento (CE) 852/2004, está basado en los principios de Análisis de Peligros y Puntos de Control Críticos (APPCC).'));
+    private function SelectReportIntro($template) {
+        $this->Paragraph('La presente Auditoría Interna Higiénico-Sanitaria se ha evaluado teniendo en cuenta distintos apartados esenciales para llevar a cabo un correcto Sistema de Autocontrol. Este sistema necesario y obligatorio para las empresas alimentarias según el Reglamento (CE) 852/2004, está basado en los principios de Análisis de Peligros y Puntos de Control Críticos (APPCC).');
+        $this->Paragraph('Dichos apartados se han puntuado atendiendo tanto a obligaciones legales como a recomendaciones técnicas que se consideran claves para garantizar la seguridad en los alimentos, de acuerdo a las referencias descritas al final del presente informe.');
+        $this->Paragraph('El modo de puntuación considerado es el siguiente:');
         $this->Ln(5);
-        $this->MultiCell(0, 5, utf8_decode('Dichos apartados se han puntuado atendiendo tanto a obligaciones legales como a recomendaciones técnicas que se consideran claves para garantizar la seguridad en los alimentos, de acuerdo a las referencias descritas al final del presente informe.'));
-        $this->Ln(5);
-        $this->MultiCell(0, 5, utf8_decode('El modo de puntuación considerado es el siguiente:'));
-        $this->Ln(5);
-        $this->SetX(60);
-        $this->SetFillColor(214, 233, 198);
-        $this->Cell(10, 7, 'A', 1, 0, 'C', true);
-        $this->Cell(50, 7, 'Satisfactorio', 1, 0, 'C', true);
-        $this->Cell(30, 7, '85 - 100', 1, 0, 'C', true);
-        $this->Ln();
-        $this->SetX(60);
-        $this->SetFillColor(250, 235, 204);
-        $this->Cell(10, 7, 'B', 1, 0, 'C', true);
-        $this->Cell(50, 7, 'Aceptable', 1, 0, 'C', true);
-        $this->Cell(30, 7, '65 - 84', 1, 0, 'C', true);
-        $this->Ln();
-        $this->SetX(60);
-        $this->SetFillColor(235, 204, 209);
-        $this->Cell(10, 7, 'C', 1, 0, 'C', true);
-        $this->Cell(50, 7, 'Necesita mejorar', 1, 0, 'C', true);
-        $this->Cell(30, 7, '0 - 64', 1, 0, 'C', true);
-        $this->SetFillColor(0, 0, 0);
-        $this->Ln(15);
-        $this->MultiCell(0, 5, utf8_decode('Además de las puntuaciones de cada apartado, en el apartado de Detalles de la auditoría indicando las oportunidades de mejora detectadas de cada premisa durante la auditoría en cada uno de ellos. También se indican en este apartado las posibles No Conformidades que el auditor ha encontrado (puntuadas como C en cada premisa).'));
-        $this->Ln(5);
-        $this->MultiCell(0, 5, utf8_decode('Se recuerda que es posible que lo detectable en la presente auditoría pueda estar sujeto a modificaciones a corto plazo por lo que es primordial la colaboración y el trabajo constante del personal responsable para llevar a cabo un adecuado Sistema de Autocontrol.'));
-        $this->Ln(5);
-        $this->MultiCell(0, 5, utf8_decode('Apthisa agradece a la dirección y empleados del establecimiento su atención y colaboración durante la auditoría.'));
+        $this->Table(
+            [
+                ['bg' => [214, 233, 198], 'values' => ['A', 'Satisfactorio', '85 - 100']],
+                ['bg' => [250, 235, 204], 'values' => ['B', 'Aceptable', '65 - 84']],
+                ['bg' => [235, 204, 209], 'values' => ['C', 'Necesita mejorar', '0 - 64']]
+            ],
+            ['height' => 7, 'width' => [10, 50, 30], 'marginLeft' => 60]
+        );
+        $this->Ln(10);
+        $this->Paragraph('Además de las puntuaciones de cada apartado, en el apartado de Detalles de la auditoría indicando las oportunidades de mejora detectadas de cada premisa durante la auditoría en cada uno de ellos. También se indican en este apartado las posibles No Conformidades que el auditor ha encontrado (puntuadas como C en cada premisa).');
+        $this->Paragraph('Se recuerda que es posible que lo detectable en la presente auditoría pueda estar sujeto a modificaciones a corto plazo por lo que es primordial la colaboración y el trabajo constante del personal responsable para llevar a cabo un adecuado Sistema de Autocontrol.');
+        $this->Paragraph('Apthisa agradece a la dirección y empleados del establecimiento su atención y colaboración durante la auditoría.');
 
-        $this->Ln(30);
+        $this->Ln(15);
         $this->Cell(20);
         $this->SetFont('Arial', 'U', 12);
         $this->Cell(100, 0, 'Realizado en');
         $this->Cell(100, 0, 'Elaborado por');
-        $this->Ln(10);
+        $this->Ln(8);
         $this->SetFont('Arial', 'B', 15);
         $this->Cell(20);
-        $this->MultiCell(100, 0, utf8_decode($this->audit->customer->name));
+        $y = $this->GetY();
+        $this->MultiCell(80, 5, utf8_decode($this->audit->customer->name), 0, 'L');
+        $this->SetY($y + 2);
         $this->SetFont('Arial', '', 12);
         $this->Cell(120);
         $this->Cell(0, 0, utf8_decode($this->audit->auditor->name));
@@ -148,50 +120,35 @@ class AuditPDF extends FPDF {
         $this->Cell(0, 0, 'Auditora de APTHISA');
     }
 
-    function SelectReportSummary($template) {
-        $this->Title('Resumen de puntuaciones');
-        $this->Ln(20);
-        $this->SetFont('Arial', '', 10);
+    private function SelectReportSummary($template) {
+        $rows = [];
         foreach($template->form_template_sections as $s) {
-            $this->SetX(20);
-            $this->Cell(150, 8, utf8_decode($s->name), 1, 0);
-            $this->Cell(15, 8, round($s->score)." ", 1, 0, 'R');
-            $this->Ln(8);
+            $rows[] = ['values' => [$s->name, $s->score]];
         }
-        $this->SetX(20);
-        $this->SetFont('Arial', 'B', 12);
-        $this->Cell(150, 8, utf8_decode('Total'), 1, 0);
-        $this->Cell(15, 8, $template->score." ", 1, 0, 'R');
-        $this->Ln(20);
-        $this->SelectReportSummaryTotalTable($template);
-    }
+        $rows[] = ['values' => ['Total', $template->score], 'font' => ['Arial', 'B', 10]];
 
-    function SelectReportSummaryTotalTable($template) {
-        $this->SetFont('Arial', 'B', 12);
-        $this->SetX(90);
-        $this->Cell(55, 8, utf8_decode('Total'), 1, 0, 'C');
-        $this->Cell(40, 8, "{$template->score}%", 1, 0, 'C');
-        $this->Ln(8);
-        $this->SetX(90);
-        $this->Cell(55, 8, utf8_decode('Puntuación'), 1, 0, 'C');
+        $this->Table(
+            $rows,
+            [ 'align' => ['L','R'], 'font' => ['Arial','',10], 'height' => 8, 'marginLeft' => 20, 'width' => [150, 15] ]
+        );
+        $this->Ln(20);
+
+        $letter = 'A';
         if($template->score < 65) {
-            $this->SetFillColor(235, 204, 209);
             $letter = 'C';
         } elseif($template->score < 85) {
-            $this->SetFillColor(250, 235, 204);
             $letter = 'B';
-        } else {
-            $this->SetFillColor(214, 233, 198);
-            $letter = 'A';
         }
-        $this->Cell(40, 8, $letter, 1, 0, 'C', true);
-        $this->SetFillColor(0, 0, 0);
+        $this->Table(
+            [
+                ['values' => ['Total', "{$template->score}%"]],
+                ['values' => ['Puntuación', $letter]]
+            ],
+            ['font' => ['Arial', 'B', 12], 'height' => 8, 'marginLeft' => 90, 'width' => [55, 40]]
+        );
     }
 
-    function SelectReportDetail($template) {
-        $this->Title('Detalles de auditoría');
-        $this->Ln(20);
-
+    private function SelectReportDetail($template) {
         $this->SetFont('Arial', 'B', 10);
         $this->SetFillColor(237, 239, 246);
         $this->SetTextColor(29, 113, 184);
@@ -255,7 +212,76 @@ class AuditPDF extends FPDF {
         }
     }
 
-    function WrapString($str, $max_length) {
+    private function Title($title) {
+        $this->SetFont('Arial', 'B', 20);
+        $this->SetTextColor(29, 113, 184);
+        $this->Cell(0, 20, utf8_decode($title), 0, 0, 'C');
+        $this->SetFont('Arial', '', 12);
+        $this->SetTextColor(0, 0, 0);
+        $this->Ln(20);
+    }
+
+    private function SubTitle($title) {
+        $this->SetFont('Arial', 'B', 18);
+        $this->Cell(0, 15, utf8_decode($title), 0, 0, 'C');
+        $this->SetFont('Arial', '', 12);
+        $this->Ln(20);
+    }
+
+    private function Paragraph($text) {
+        $this->MultiCell(0, 5, utf8_decode($text));
+        $this->Ln(5);
+    }
+
+    private function Table($rows, $config) {
+        foreach($rows as $row) {
+            if(!empty($config['marginLeft'])) {
+                $this->SetX($config['marginLeft']);
+            }
+            $hasBgColor = !empty($row['bg']);
+            if($hasBgColor) {
+                $this->SetFillColor($row['bg'][0], $row['bg'][1], $row['bg'][2]);
+            }
+            $hasTextColor = !empty($row['color']);
+            if($hasTextColor) {
+                $this->SetTextColor($row['color'][0], $row['color'][1], $row['color'][2]);
+            }
+            if(!empty($config['font'])) {
+                $this->SetFont($config['font'][0], $config['font'][1], $config['font'][2]);
+            }
+            if(!empty($row['font'])) {
+                $this->SetFont($row['font'][0], $row['font'][1], $row['font'][2]);
+            }
+            foreach($config['width'] as $i => $w) {
+                $align = 'C';
+                if(!empty($config['align'][$i])) {
+                    $align = $config['align'][$i];
+                }
+                if(!empty($row['align'][$i])) {
+                    $align = $row['align'][$i];
+                }
+                if(is_array($row['values'][$i])) {
+                    if($row['values'][$i]['type'] === 'img') {
+                        $x = $this->GetX();
+                        $y = $this->GetY();
+                        $this->Image($row['values'][$i]['path'], $x+($w/2)-($row['values'][$i]['width']/2), $y+1, $row['values'][$i]['width']);
+                        $this->Cell($w, $config['height'], '', 1);
+                    }
+                } else {
+                    $this->Cell($w, $config['height'], utf8_decode($row['values'][$i]), 1, 0, $align, $hasBgColor);
+                }
+            }
+            if($hasBgColor) {
+                $this->SetFillColor(255, 255, 255);
+            }
+            if($hasTextColor) {
+                $this->SetTextColor(0, 0, 0);
+            }
+            $this->Ln();
+        }
+    }
+
+    private function WrapString($str, $max_length) {
         $result = [];
         $current = "";
         $words = explode(" ", strip_tags($str));
@@ -271,7 +297,7 @@ class AuditPDF extends FPDF {
         return $result;
     }
 
-    function WrappedStringTableCell($w, $h, $wrappedStr, $fill=false) {
+    private function WrappedStringTableCell($w, $h, $wrappedStr, $fill=false) {
         $x = $this->GetX();
         $y = $this->GetY();
         foreach($wrappedStr as $i => $str) {
