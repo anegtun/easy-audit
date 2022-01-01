836d4ab51cdd8f4650643019c2376c0b3ce44e2a
Angel Negrete <angel.negrete.tunas@gmail.com>
2022-01-01 17:16:59 +0100

    Improve collapse code

--- app/webroot/js/collapse-unique.js
+++ app/webroot/js/collapse-unique.js
@@ -1,19 +1,21 @@
 $(document).ready(function() {
-    
+
+    $('[data-toggle="collapse-unique"]').each(function(i, item) {
+        const clicked = this;
+        $(clicked.hash).on('shown.bs.collapse', function() {
+            $('html, body').animate({ scrollTop: $(clicked).offset().top }, 200);
+        }).on('show.bs.collapse', function() {
+            $('html, body').animate({ scrollTop: $(clicked).offset().top }, 200);
+        });
+    });
+
     $('[data-toggle="collapse-unique"]').click(function(e) {
         e.preventDefault();
-        const clicked = this;
-        const target = $(clicked.hash);
+        const target = $(this.hash);
         if(!target.hasClass('in')) {
-            $('[data-toggle="collapse-unique"]').each(function(i, item) {
-                $(item.hash).collapse('hide');
-            });
+            $('[data-toggle="collapse-unique"]').each((i, item) => $(item.hash).collapse('hide'));
         }
-        target.on('shown.bs.collapse', function() {
-            $('html, body').animate({
-                scrollTop: $(clicked).offset().top
-            }, 200);
-        }).collapse('toggle');
+        target.collapse('toggle');
     });
 
 });
